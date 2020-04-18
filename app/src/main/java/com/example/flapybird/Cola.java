2c664dc22299a7a2a41fa1075af0a3086a72358e
Mark Jin <xjin13@illinois.edu>
2020-04-18 22:49:26 +0800

    HI

--- app/src/main/java/com/example/flapybird/Cola.java
+++ app/src/main/java/com/example/flapybird/Cola.java
@@ -1,6 +1,7 @@
 package com.example.flapybird;
 
 public class Cola {
-    System.out.println("jjj");
-    System.out.println("asdfghjkl");
+    //System.out.println("jjj");
+    //System.out.println("asdfghjkl");
+    private int i = 0;
 }
