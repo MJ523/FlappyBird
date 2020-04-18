be74ea2ef2b6a5ec992d7a4fa49f2a83a746275d
Yi Liu <yiliu18@illinois.edu>
2020-04-18 09:50:39 -0500

    corona

--- app/src/main/java/com/example/flapybird/Cola.java
+++ app/src/main/java/com/example/flapybird/Cola.java
@@ -3,5 +3,6 @@ package com.example.flapybird;
 public class Cola {
     //System.out.println("jjj");
     //System.out.println("asdfghjkl");
-    private int i = 0;
+    // private int i = 0;
+    coronavirus
 }
