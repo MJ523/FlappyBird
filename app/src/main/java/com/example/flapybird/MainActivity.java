dbb88645c49f133ef08dd567b191a3c1b590d818
Yi Liu <yiliu18@illinois.edu>
2020-04-28 11:28:52 -0500

    added two screens and partially functional buttons

--- app/src/main/java/com/example/flapybird/MainActivity.java
+++ app/src/main/java/com/example/flapybird/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.flapybird;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.game_screen);
+            }
+        });
     }
+
 }
